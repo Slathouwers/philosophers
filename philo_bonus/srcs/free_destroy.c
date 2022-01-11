535727ddf6b02140ae0ebb067ae3d0aa7a532622
Slathouwers <sander.lathouwers@live.be>
2022-01-11 13:27:26 +0000

    implemented cleanup after execution

--- philo_bonus/srcs/free_destroy.c
+++ philo_bonus/srcs/free_destroy.c
@@ -6,37 +6,57 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 10:27:51 by slathouw          #+#    #+#             */
-/*   Updated: 2022/01/10 18:34:01 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/11 13:09:07 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-void	destroy_mutex(t_dinner *d);
+void	kill_all_philoprocs(t_dinner *d);
+void	close_semaphores(t_dinner *d);
 void	free_all(t_dinner *d);
 
-void	destroy_mutex(t_dinner *d)
+void	kill_all_philoprocs(t_dinner *d)
 {
 	int	i;
 
 	i = -1;
 	while (++i < d->n_philos)
+		kill(d->philo_arr[i].pid, SIGKILL);
+	i = -1;
+	while (++i < d->n_philos)
 	{
-		pthread_mutex_unlock(&d->philo_arr[i].mealtime_lock);
-		pthread_mutex_destroy(&d->philo_arr[i].mealtime_lock);
-		pthread_mutex_unlock(&d->fork_arr[i]);
-		pthread_mutex_destroy(&d->fork_arr[i]);
+		sem_close(d->philo_arr[i].sem_lunch);
+		sem_unlink(d->philo_arr[i].lname);
+		free(d->philo_arr[i].lname);
+		d->philo_arr[i].lname = NULL;
 	}
-	pthread_mutex_unlock(&d->printer);
-	pthread_mutex_destroy(&d->printer);
+	free(d->philo_arr);
+	d->philo_arr = NULL;
+}
+
+void	close_semaphores(t_dinner *d)
+{
+	sem_close(d->sem_forks);
+	sem_unlink("forks");
+	sem_close(d->sem_start);
+	sem_unlink("start");
+	sem_close(d->sem_printer);
+	sem_unlink("printer");
 }
 
 void	free_all(t_dinner *d)
 {
-	if (d->fork_arr)
-		free(d->fork_arr);
-	if (d->philo_arr)
-		free(d->philo_arr);
-	if (d->life_threads)
-		free(d->life_threads);
+	int	i;
+
+	i = -1;
+	if (!d)
+		return ;
+	if (!d->philo_arr)
+		return ;
+	while (++i < d->n_philos)
+		if (d->philo_arr[i].lname)
+			free(d->philo_arr[i].lname);
+	free(d->philo_arr);
+	d->philo_arr = NULL;
 }
