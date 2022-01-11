924749ed8d99d07f19d7a4b96873d9ecee088c7a
Slathouwers <sander.lathouwers@live.be>
2022-01-11 13:28:06 +0000

    implemented initializers, subprocess launcher and monitoring function

--- philo_bonus/srcs/init.c
+++ philo_bonus/srcs/init.c
@@ -6,16 +6,17 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 10:20:57 by slathouw          #+#    #+#             */
-/*   Updated: 2022/01/10 18:32:01 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/11 13:13:09 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-int	init_dinner(t_dinner *d, int ac, char **av);
-int	init_mutex(t_dinner *d);
-int	init_philos(t_dinner *dinner);
-int	init_threads(t_dinner *d);
+int		init_dinner(t_dinner *d, int ac, char **av);
+int		init_semaphores(t_dinner *d);
+int		init_philos(t_dinner *d);
+void	monitor_philos(t_dinner *d);
+int		launch_philos(t_dinner *d);
 
 int	init_dinner(t_dinner *d, int ac, char **av)
 {
@@ -24,7 +25,6 @@ int	init_dinner(t_dinner *d, int ac, char **av)
 	d->t_to_die = ft_atoi(av[2]);
 	d->t_to_eat = ft_atoi(av[3]);
 	d->t_to_sleep = ft_atoi(av[4]);
-	d->finished = 0;
 	if (ac == 5)
 		d->min_n_meals = -1;
 	else
@@ -32,72 +32,82 @@ int	init_dinner(t_dinner *d, int ac, char **av)
 	return (1);
 }
 
-int	init_mutex(t_dinner *d)
+int	init_semaphores(t_dinner *d)
 {
-	int				n_philos;
-	pthread_mutex_t	*forks;
-
-	n_philos = d->n_philos;
-	forks = malloc(sizeof(pthread_mutex_t) * n_philos);
-	if (!forks)
+	sem_unlink("printer");
+	d->sem_printer = sem_open("printer", O_CREAT, S_IRWXU | S_IRWXG, 1);
+	sem_unlink("start");
+	d->sem_start = sem_open("start", O_CREAT, S_IRWXU | S_IRWXG, 0);
+	sem_unlink("forks");
+	d->sem_forks = sem_open("forks", O_CREAT, S_IRWXU | S_IRWXG, d->n_philos);
+	if (!d->sem_forks || !d->sem_printer || !d->sem_start)
 		return (0);
-	while (n_philos--)
-		pthread_mutex_init(&forks[n_philos], NULL);
-	pthread_mutex_init(&d->printer, NULL);
-	d->fork_arr = forks;
 	return (1);
 }
 
-int	init_philos(t_dinner *dinner)
+int	init_philos(t_dinner *d)
 {
 	int		i;
-	t_philo	*phil_arr;
 
 	i = -1;
-	phil_arr = (t_philo *) malloc(sizeof(t_philo) * dinner->n_philos);
-	if (!phil_arr)
+	while (++i < d->n_philos)
 	{
-		if (dinner->fork_arr)
-			free(dinner->fork_arr);
-		return (0);
+		ft_bzero(&d->philo_arr[i], sizeof(t_philo));
+		d->philo_arr[i].id = i;
+		d->philo_arr[i].d = d;
+		d->philo_arr[i].lname = get_lunch_name(i);
+		sem_unlink(d->philo_arr[i].lname);
+		d->philo_arr[i].sem_lunch = sem_open(d->philo_arr[i].lname, O_CREAT,
+				S_IRWXU | S_IRWXG, 1);
+		if (!d->philo_arr[i].sem_lunch)
+			return (0);
 	}
-	while (++i < dinner->n_philos)
-	{
-		ft_bzero(&phil_arr[i], sizeof(t_philo));
-		phil_arr[i].id = i;
-		phil_arr[i].l_fork = &dinner->fork_arr[i];
-		phil_arr[i].r_fork = &dinner->fork_arr[(i + 1) % dinner->n_philos];
-		phil_arr[i].d = dinner;
-		pthread_mutex_init(&phil_arr[i].mealtime_lock, NULL);
-	}
-	dinner->philo_arr = phil_arr;
 	return (1);
 }
 
-int	init_threads(t_dinner *d)
+void	monitor_philos(t_dinner *d)
 {
-	int			n_philos;
-	pthread_t	*life_threads;
-	pthread_t	grim_reaper;
+	int	n_philos_full;
+	int	exit_status;
 
-	n_philos = d->n_philos;
-	d->tstamp_start = get_tstamp();
-	life_threads = (pthread_t *) malloc(sizeof(pthread_t) * n_philos);
-	if (!life_threads)
+	n_philos_full = 0;
+	if (d->min_n_meals > 0)
 	{
-		free_all(d);
+		while (n_philos_full < d->n_philos)
+		{
+			waitpid(-1, &exit_status, 0);
+			if (WEXITSTATUS(exit_status) == EXIT_PHILO_DIED)
+				break ;
+			n_philos_full++;
+		}
+		if (n_philos_full == d->n_philos)
+			printf("All philo's full..\n");
+	}
+	else
+		waitpid(-1, &exit_status, 0);
+	kill_all_philoprocs(d);
+	close_semaphores(d);
+}
+
+int	launch_philos(t_dinner *d)
+{
+	int	i;
+
+	i = -1;
+	d->philo_arr = malloc(sizeof(t_philo) * d->n_philos);
+	if (!d->philo_arr || !init_philos(d))
 		return (0);
-	}
-	while (n_philos--)
+	d->tstamp_start = get_tstamp();
+	while (++i < d->n_philos)
 	{
-		d->philo_arr[n_philos].tstamp_last_meal = get_tstamp();
-		pthread_create(&life_threads[n_philos], NULL,
-			&life, (void *)&d->philo_arr[n_philos]);
-		pthread_detach(life_threads[n_philos]);
+		d->philo_arr[i].pid = fork();
+		d->philo_arr[i].tstamp_last_meal = d->tstamp_start;
+		if (!d->philo_arr[i].pid)
+			reap_death(&d->philo_arr[i]);
+		else if (d->philo_arr[i].pid < 0)
+			return (0);
 	}
-	pthread_create(&grim_reaper, NULL, &reap_death, (void *)d->philo_arr);
-	pthread_join(grim_reaper, NULL);
-	//reap_death(d->philo_arr);
-	d->life_threads = life_threads;
+	post_start(d);
+	monitor_philos(d);
 	return (1);
 }
