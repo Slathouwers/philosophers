36cd94e9c15022e4055555a23bf0a751d4002672
Slathouwers <sander.lathouwers@live.be>
2022-01-11 13:29:03 +0000

    cleanup ft_atoi

--- philo_bonus/srcs/utils.c
+++ philo_bonus/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:45:56 by slathouw          #+#    #+#             */
-/*   Updated: 2022/01/10 18:32:20 by slathouw         ###   ########.fr       */
+/*   Updated: 2022/01/11 08:28:56 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,15 @@ time_t	get_tstamp(void)
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	i;
-	int	num;
+	int				sign;
+	int				i;
+	unsigned int	num;
 
 	if (!str)
 		return (0);
 	i = 0;
 	sign = 1;
 	num = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		num = num * 10 + (str[i] - '0');
